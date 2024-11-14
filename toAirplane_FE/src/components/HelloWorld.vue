5d60c2a2e6ed7d368b6d122c85c1df8ff944dac4
jh.nam0324 <jwonny0118@gmail.com>
2024-11-14 17:02:11 +0900

    구글 폰트 삽입

--- toAirplane_FE/src/components/HelloWorld.vue
+++ toAirplane_FE/src/components/HelloWorld.vue
@@ -12,6 +12,7 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
+      안녕!
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
--- toAirplane_FE/src/main.js
+++ toAirplane_FE/src/main.js
@@ -1,3 +1,5 @@
+import './main.css'
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
