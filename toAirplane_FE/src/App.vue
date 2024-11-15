9d0a7773dff127155142e92b01852a4feee3315d
jh.nam0324 <jwonny0118@gmail.com>
2024-11-15 17:28:12 +0900

    병합 테스트1

--- toAirplane_FE/src/App.vue
+++ toAirplane_FE/src/App.vue
@@ -5,11 +5,18 @@ import Main from './components/Main.vue';
 </script>
 
 <template>
-  <HeaderNav></HeaderNav> <!-- 헤더 -->
-  <Main></Main> <!-- 메인 콘텐츠 -->
-  <Footer></Footer> <!-- 푸터 -->
-  <!-- <div id="app">
-  </div> -->
+  <HeaderNav @open-signin="openSignInModal"></HeaderNav>
+    <!-- 로그인 모달-->
+    <SignInModal 
+      @openSignUpModal="openSignUpModal" 
+      :showSignInModal="showSignInModal" 
+      @closeSignInModal="closeSignInModal"
+      @openFindIdModal="openFindIdModal"  />
+    <!-- 회원가입 모달 -->
+    <SignUpModal  :showSignUpModal="showSignUpModal" @closeSignUpModal="closeSignUpModal"/>
+    <FindIdModal  :showFindIdModal="showFindIdModal" @closeFindIdModal="closeFindIdModal"/>
+    <Main></Main>
+    <Footer></Footer>
 </template>
 
 <style scoped></style>
